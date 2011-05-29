09e09eb36f320d681e497a62ae98abee300a6fe2
Yamada Taro <tsuyoppy@r2.dion.ne.jp>
2011-05-29 09:24:36 +0900

    added

---
+++ apps/browser/firefox/extension/helloworld/skeleton@example.ne.jp/chrome/content/ff-overlay.js
@@ -0,0 +1,19 @@
+﻿{
+	let tm;
+	var skeletonNs={
+		// グローバル・オブジェクトを書く
+		onMenuItemCommand:function(){
+
+
+			alert('Hello World1');
+			window.open(" chrome://skeleton/content/prefs.xul", "bmarks", "chrome,width=600,height=300");
+		}
+	};
+
+	let init=function() {
+		window.clearTimeout(tm);
+		// 拡張の初期化処理を書く
+	};
+
+	tm=window.setTimeout(init,500);
+}
---
+++ apps/browser/firefox/extension/helloworld/skeleton@example.ne.jp/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.skeleton.boolpref", false);
+pref("extensions.skeleton.intpref", 0);
+pref("extensions.skeleton.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.skeleton@example.ne.jp.description", "chrome://skeleton/locale/overlay.properties");

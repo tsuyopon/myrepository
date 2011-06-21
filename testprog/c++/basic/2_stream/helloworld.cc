70af17831efe2e69a64674dcef2441131b4fe241
Your Name <tsuyoppy@r2.dion.ne.jp>
2011-06-21 10:40:08 +0900

    initial version

---
+++ testprog/c++/basic/2_stream/helloworld.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char* name = "tsuyopon";
+	cout << "Hello " << name << endl;
+	cerr << "STANDARD OUTPUT ERROR" << endl;
+	return EXIT_SUCCESS;
+}

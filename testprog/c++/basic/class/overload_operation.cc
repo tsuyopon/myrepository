1ebc3d0984db7b5b8db9df2dd46aaaebbb1f4a0b
Your Name <tsuyoppy@r2.dion.ne.jp>
2011-06-25 20:57:35 +0900

    added

--- testprog/c++/basic/class/overload_operation.cc
+++ testprog/c++/basic/class/overload_operation.cc
@@ -9,13 +9,16 @@ class Integer {
 	private:
 		int value;
 	public:
-		int operator + (Integer obj){
+		int operator + (Integer obj){         // +演算子のオーバーロードに関する定義
+			cout << "Plus Operator START" << endl;
 			return 777;
 		}
-		int operator - (Integer obj){
+		int operator - (Integer obj){         // -演算子のオーバーロードに関する定義
+			cout << "Minus Operator START" << endl;
 			return this->value - obj.value;
 		}
 		Integer(int value){
+			cout << "Integer(" << value << ") START" << endl;
 			this->value = value;
 		}
 };
@@ -23,6 +26,7 @@ class Integer {
 int main(){
 	Integer obj1(10), obj2(100);
 	cout << obj1 + obj2 << endl;
+	cout << obj1 - obj2 << endl;
 	cout << obj1 - obj2 << endl;
 	return 0;
 }
